a3c9d6fac9c5be0b73182726e316e20b59f3b1a7
IamSumukha <sumukh.aithal@gmail.com>
2021-01-26 14:34:52 +0530

    "added signup/login page"

--- app/src/main/java/com/johnnybkotlin/smack/MainActivity.kt
+++ app/src/main/java/com/johnnybkotlin/smack/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.johnnybkotlin.smack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.johnnybkotlin.smack.ui.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
 
     }
+
+
+
+
+    fun loginBtnNavClicked(view: View) {
+
+        val loginIntent = Intent(this,LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
+
+    fun addChannelBtnClicked(view: View) {}
+
+    fun sendmessageOnclick(view: View) {}
+
 }
---
+++ app/src/main/java/com/johnnybkotlin/smack/ui/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.johnnybkotlin.smack.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.johnnybkotlin.smack.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginBtnClicked(view: View) {}
+    fun createuserbtnclick(view: View) {}
+}
